936095e29aded0b5aa2448bee75b44cd914194f6
schlechtums <schlechtums@gmail.com>
2021-08-30 16:27:45 -0400

    Feature/rolling window (#6)
    Created RollingWindow class

---
+++ src/Schlechtums.Core/Schlechtums.Core/Common/RollingWindow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Schlechtums.Core.Common
+{
+    public class RollingWindow<T> : IEnumerable<T>
+    {
+        public RollingWindow(int windowSize)
+        {
+            this._WindowSize = windowSize;
+            this._Queue = new Queue<T>(windowSize);
+        }
+
+        private int _WindowSize;
+        private Queue<T> _Queue;
+
+        public void Add(T item)
+        {
+            if (this._Queue.Count == this._WindowSize)
+                this._Queue.Dequeue();
+
+            this._Queue.Enqueue(item);
+        }
+
+        public void Add(params T[] items)
+        {
+            foreach (var i in items)
+            {
+                this.Add(i);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_Queue).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_Queue).GetEnumerator();
+        }
+    }
+}
